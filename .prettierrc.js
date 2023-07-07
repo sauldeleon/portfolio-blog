d9534faf82affe7ed3a0d36a6d4da958b530ffeb
Saúl de León Guerrero <saul.deleon@bonhams.com>
2023-07-07 10:09:35 +0200

    feat: last configuration

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+/* istanbul ignore file */
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  importOrder: ['<THIRD_PARTY_MODULES>', '^@nx/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+}
