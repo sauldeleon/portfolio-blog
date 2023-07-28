caf4880d738d4e271dbe33c266720fbd860d925e
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2023-07-28 13:49:48 +0200

    Feat/initialize api (#4)
    * feat: add initial gin server
    
    * feat: add initial gin server
    
    * feat: fix pr comments
    
    ---------
    
    Co-authored-by: Saúl de León Guerrero <saul.deleon@bonhams.com>

--- apps/api/main.go
+++ apps/api/main.go
@@ -1,12 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-func Hello(name string) string {
-	result := "Hello " + name
-	return result
+	"github.com/gin-gonic/gin"
+)
+
+var db = make(map[string]string)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	r.GET("/user/:name", func(c *gin.Context) {
+		user := c.Params.ByName("name")
+		value, ok := db[user]
+		if ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
+		}
+	})
+
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+		"foo":  "bar",
+		"test": "123",
+	}))
+
+	authorized.POST("admin", func(c *gin.Context) {
+		user := c.MustGet(gin.AuthUserKey).(string)
+
+		var json struct {
+			Value string `json:"value" binding:"required"`
+		}
+
+		if c.Bind(&json) == nil {
+			db[user] = json.Value
+			c.JSON(http.StatusOK, gin.H{"status": "ok"})
+		}
+	})
+
+	return r
 }
 
 func main() {
-	fmt.Println(Hello("api"))
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
 }
--- apps/api/main_test.go
+++ apps/api/main_test.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestHello(t *testing.T) {
-	result := Hello("world")
-	if result != "Hello world" {
-		t.Error("Expected Hello to append 'world'")
-	}
+func TestPingRoute(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "pong", w.Body.String())
+}
+
+func TestUserName(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/user/test", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "{\"status\":\"no value\",\"user\":\"test\"}", w.Body.String())
 }
