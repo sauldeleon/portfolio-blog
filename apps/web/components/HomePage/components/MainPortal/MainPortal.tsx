be2f8b0e89e187eda18765ff461e76d2bddf9bfe
Saúl de León Guerrero <saul.deleon@bonhams.com>
2023-08-22 11:06:38 +0200

    feat: add logos

--- apps/web/components/HomePage/components/MainPortal/MainPortal.tsx
+++ apps/web/components/HomePage/components/MainPortal/MainPortal.tsx
@@ -1,4 +1,17 @@
-import { NodeJSIcon } from '@sdlgr/assets'
+import {
+  CSS3Icon,
+  GithubIcon,
+  JestIcon,
+  MongoDBIcon,
+  NextJSIcon,
+  NodeJSIcon,
+  NxIcon,
+  ReactJSIcon,
+  ReactQueryIcon,
+  TypescriptIcon,
+  XataIOIcon,
+  YarnIcon,
+} from '@sdlgr/assets'
 
 import { AnimatedItem, Portals } from '@web/components/Portals/Portals'
 
@@ -12,22 +25,70 @@ import {
 export function MainPortal() {
   const items: AnimatedItem[] = [
     {
-      svg: <NodeJSIcon color="red" />,
+      svg: <NodeJSIcon />,
       rotate: true,
       colorSwap: true,
     },
-    // { path: '/assets/react-logo.png', rotate: true, size: 'S' },
-    // { path: '/assets/github-logo.png', size: 'M' },
-    // { path: '/assets/reactQuery-logo.png', rotate: true },
-    // { path: '/assets/nextjs-logo.png' },
-    // { path: '/assets/css3-logo.png' },
-    // { path: '/assets/typescript-logo.png' },
-    // {},
-    // {},
-    // {},
-    // {},
-    // {},
-    // {},
+    {
+      svg: <CSS3Icon />,
+      rotate: true,
+      colorSwap: true,
+      size: 'S',
+    },
+    {
+      svg: <GithubIcon />,
+      rotate: true,
+      colorSwap: true,
+      size: 'S',
+    },
+    {
+      svg: <TypescriptIcon />,
+      rotate: true,
+      colorSwap: true,
+      size: 'S',
+    },
+    {
+      svg: <NextJSIcon />,
+      rotate: true,
+      colorSwap: true,
+    },
+    {
+      svg: <ReactJSIcon />,
+      rotate: true,
+      colorSwap: true,
+    },
+    {
+      svg: <MongoDBIcon />,
+      rotate: true,
+      colorSwap: true,
+      size: 'S',
+    },
+    {
+      svg: <ReactQueryIcon />,
+      rotate: true,
+    },
+    {
+      svg: <JestIcon />,
+      rotate: true,
+      colorSwap: true,
+    },
+    {
+      svg: <NxIcon />,
+      rotate: true,
+      size: 'S',
+      colorSwap: true,
+    },
+    {
+      svg: <YarnIcon />,
+      rotate: true,
+      size: 'S',
+      colorSwap: true,
+    },
+    {
+      svg: <XataIOIcon />,
+      rotate: true,
+      size: 'S',
+    },
   ]
 
   return (
