55c0f331e653bc6404ab745935f3864e5636a693
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2023-08-24 00:46:14 +0200

    test: improve coverage

--- apps/web/components/MainLayout/components/Footer/Footer.spec.tsx
+++ apps/web/components/MainLayout/components/Footer/Footer.spec.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { renderApp } from '@sdlgr/test-utils'
 
@@ -11,5 +12,17 @@ describe('Footer', () => {
     expect(screen.getAllByRole('link')).toHaveLength(6)
     expect(screen.getAllByRole('button')).toHaveLength(2)
     expect(baseElement).toMatchSnapshot()
+  })
+
+  it('should trigger actions in buttons successfully', async () => {
+    jest.spyOn(console, 'log')
+    const mockFn = jest.fn()
+    ;(console.log as jest.Mock).mockImplementation(mockFn)
+
+    renderApp(<Footer />)
+    await screen.findByRole('navigation')
+    await userEvent.click(screen.getByRole('button', { name: /Dark mode/ }))
+    await userEvent.click(screen.getByRole('button', { name: /Pain mode/ }))
+    expect(mockFn).toHaveBeenCalledTimes(2)
   })
 })
