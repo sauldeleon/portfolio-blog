227a1af06df46722dbdc3ae265e234a635939675
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2023-08-25 14:58:15 +0200

    feat: add progress bar

--- apps/web/components/MainLayout/MainLayout.tsx
+++ apps/web/components/MainLayout/MainLayout.tsx
@@ -1,5 +1,9 @@
 'use client'
 
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar'
+
+import { mainTheme } from '@sdlgr/main-theme'
+
 import { StyledContent, StyledPage } from './MainLayout.styles'
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
@@ -11,6 +15,15 @@ interface LayoutProps {
 export function MainLayout({ children }: LayoutProps) {
   return (
     <StyledPage>
+      <ProgressBar
+        color={mainTheme.colors.white}
+        height="1px"
+        options={{
+          showSpinner: false,
+          template: '<div class="bar" role="bar"><div class="peg"></div></div>',
+        }}
+        shallowRouting
+      />
       <Header />
       <StyledContent>{children}</StyledContent>
       <Footer />
