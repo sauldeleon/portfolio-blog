243fe2c1e7826ca10a6282bcfa3cde2a3f6e49cf
Saúl de León Guerrero <saul.deleon@bonhams.com>
2023-09-05 11:54:01 +0200

    fix: added metadata in root index page

--- apps/web/app/layout.spec.tsx
+++ apps/web/app/layout.spec.tsx
@@ -1,10 +1,29 @@
 import { render, screen } from '@testing-library/react'
 
-import RootLayout from './layout.next'
+import RootLayout, { generateMetadata } from './layout.next'
 
 describe('RootLayout', () => {
   it('should render children', async () => {
     render(<RootLayout>test</RootLayout>)
     expect(screen.getByText('test')).toBeInTheDocument()
+  })
+})
+
+describe('/ route - metadata', () => {
+  it('should generate metadata successfully', async () => {
+    expect(await generateMetadata()).toEqual({
+      title: 'Saúl de León Guerrero',
+      description: 'My personal Portfolio',
+      colorScheme: 'dark',
+      metadataBase: expect.any(Object),
+      alternates: {
+        canonical: '/en',
+        languages: {
+          'en-UK': '/en',
+          'en-US': '/en',
+          'es-ES': '/es',
+        },
+      },
+    })
   })
 })
