fe8bf5648bad0280bb0e469b491c3c06d85d3cac
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2023-09-07 19:55:33 +0200

    feat: add improved attribues in about info

--- apps/web/app/[lng]/(home)/layout.next.tsx
+++ apps/web/app/[lng]/(home)/layout.next.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useContext, useId } from 'react'
+import React, { useContext, useId, useMemo } from 'react'
 import { Trans } from 'react-i18next'
 
 import {
@@ -16,6 +16,9 @@ import { Body, Label } from '@sdlgr/typography'
 import { useClientTranslation } from '@web/i18n/client'
 
 import {
+  StyledAttribute,
+  StyledAttributeContent,
+  StyledAttributes,
   StyledCircleLink,
   StyledHeading,
   StyledSubHeading,
@@ -29,21 +32,76 @@ interface HomeLayoutProps {
 export default function HomeLayout({ children }: HomeLayoutProps) {
   const { t } = useClientTranslation('homepage')
   const { language } = useContext(LanguageContext)
+
+  const attributes = useMemo(
+    () => [
+      {
+        key: 'mountaineer',
+        element: (
+          <Trans
+            key="mountaineer-translate"
+            t={t}
+            i18nKey="attributes.mountaineer"
+            components={{
+              icon: <HikeIcon width={22} height={22} />,
+            }}
+          />
+        ),
+      },
+      {
+        key: 'petter',
+        element: (
+          <Trans
+            key="petter-translate"
+            t={t}
+            i18nKey="attributes.petter"
+            components={{
+              icon: <CatIcon width={22} height={22} />,
+            }}
+          />
+        ),
+      },
+      {
+        key: 'gamer',
+        element: (
+          <Trans
+            key="gamer-translate"
+            t={t}
+            i18nKey="attributes.gamer"
+            components={{
+              icon: <GameControllerIcon width={22} height={22} />,
+            }}
+          />
+        ),
+      },
+      {
+        key: 'photographer',
+        element: (
+          <Trans
+            key="photographer-translate"
+            t={t}
+            i18nKey="attributes.photographer"
+            components={{
+              icon: <CameraIcon width={22} height={22} />,
+            }}
+          />
+        ),
+      },
+    ],
+    [t]
+  )
+
   const summaryParagraphs: React.ReactNode[] = [
     t('summary.p1'),
     t('summary.p2'),
     t('summary.p3'),
-    <Trans
-      key="summaryP4"
-      t={t}
-      i18nKey="summary.p4"
-      components={{
-        hiker: <HikeIcon width={22} height={22} />,
-        cat: <CatIcon width={22} height={22} />,
-        gameController: <GameControllerIcon width={22} height={22} />,
-        camera: <CameraIcon width={22} height={22} />,
-      }}
-    />,
+    <StyledAttributes key="attributes">
+      {attributes.map(({ key, element }, index) => (
+        <StyledAttribute key={`${key}-attribute`}>
+          <StyledAttributeContent>{element}</StyledAttributeContent>
+        </StyledAttribute>
+      ))}
+    </StyledAttributes>,
   ]
   const id = useId()
   return (
