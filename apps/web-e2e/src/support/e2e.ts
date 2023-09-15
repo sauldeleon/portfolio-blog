01fc2c47f8c4a6b62c983384aa83cf915c8dd82e
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2023-09-15 11:01:50 +0200

    fix: add workflow parameters

--- apps/web-e2e/src/support/e2e.ts
+++ apps/web-e2e/src/support/e2e.ts
@@ -14,3 +14,8 @@
 // ***********************************************************
 // Import commands.js using ES2015 syntax:
 import './commands'
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  console.log('Uncaught exception', err, runnable.body)
+  return false
+})
